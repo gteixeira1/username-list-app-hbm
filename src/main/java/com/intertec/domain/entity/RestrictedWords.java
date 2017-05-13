7f6ca887a1e636a167ff62151b708d41cc748023
Guilherme Teixeira <Guilherme_Teixeira@gap.com>
2017-05-13 20:34:06 -0300

    Created first hibernate configurations and mapping files

---
+++ src/main/java/com/intertec/domain/entity/RestrictedWords.java
@@ -0,0 +1,24 @@
+package com.intertec.domain.entity;
+
+public class RestrictedWords {
+
+    private int id;
+
+    private String word;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getWord() {
+        return word;
+    }
+
+    public void setWord(String word) {
+        this.word = word;
+    }
+}
---
+++ src/main/java/com/intertec/domain/entity/Username.java
@@ -0,0 +1,24 @@
+package com.intertec.domain.entity;
+
+public class Username {
+
+    private int id;
+
+    private String usename;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getUsename() {
+        return usename;
+    }
+
+    public void setUsename(String usename) {
+        this.usename = usename;
+    }
+}
